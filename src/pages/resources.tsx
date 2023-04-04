23027d2c075b13da5c575a4d5fbff4da449a5c54
trentphoto <trentphoto@gmail.com>
2023-04-04 19:16:25 -0400

    build: resources page update

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -22,7 +22,7 @@ export default function ResourcesPage() {
           <div className='absolute inset-0 z-10 bg-gray-950/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-gray-950/95 sm:to-gray-900/25'></div>
 
           <div className='container relative z-20 mx-auto px-4 py-32 sm:px-6 lg:flex lg:items-center lg:px-8'>
-            <div className='max-w-xl text-center sm:text-left'>
+            <div className='max-w-2xl text-center sm:text-left'>
               <h1 className='text-3xl font-extrabold sm:text-5xl'>
                 Turn your iPhone into a
                 <strong className='block font-extrabold text-blue-600'>
@@ -31,8 +31,9 @@ export default function ResourcesPage() {
               </h1>
 
               <p className='mt-4 max-w-lg sm:text-xl sm:leading-relaxed'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Nesciunt illo tenetur fuga ducimus numquam ea!
+                With our groundbreaking app, you can turn your iPhone into a
+                productivity superpower. TasKonnect is the only app that
+                combines a powerful task manager with a powerful calendar.
               </p>
 
               <div className='mt-8 flex flex-wrap gap-4 text-center'>
